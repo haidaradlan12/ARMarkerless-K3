40872cac65d1a52306a8a64b239feeb6462b14bf
Haidaradlan12 <adlannufahaidar12@gmail.com>
2024-01-22 16:48:53 +0700

    te

---
+++ Assets/Asset Script/Gaze.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour
+{
+    List<InfoBehavior> infos = new List<InfoBehavior>();
+    public GameObject O1, O2, O3, O4, O5, O6, O7, O8;
+    private Animation A1, A2, A3, A4, A5, A6, A7, A8;
+    void Start()
+    {
+        infos = FindObjectsOfType<InfoBehavior>().ToList();
+        A1 = O1.GetComponent<Animation>();
+        A2 = O2.GetComponent<Animation>();
+        A3 = O3.GetComponent<Animation>();
+        A4 = O4.GetComponent<Animation>();
+        A5 = O5.GetComponent<Animation>();
+        A6 = O6.GetComponent<Animation>();
+        A7 = O7.GetComponent<Animation>();
+        A8 = O8.GetComponent<Animation>();
+    }
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+        {
+            GameObject go = hit.collider.gameObject;
+            if (go.CompareTag("O1"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+                A1.Play("Airplugh");
+            }
+            else if (go.CompareTag("O2"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+                A2.Play("Sepatu Safety");
+            }
+            else if (go.CompareTag("O3"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+                A3.Play("Face Shield");
+            }
+            else if (go.CompareTag("O4"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+                A4.Play("Rompi");
+            }
+            else if (go.CompareTag("O5"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+                A5.Play("Glove");
+            }
+            else if (go.CompareTag("O6"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+                A6.Play("Kacamata");
+            }
+            else if (go.CompareTag("O7"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+                A7.Play("Helm");
+            }
+            else if (go.CompareTag("O8"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+                A8.Play("Mask");
+            }
+            else
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+            }
+        }
+        else
+        {
+            CloseAll();
+            A1.Stop();
+            A2.Stop();
+            A3.Stop();
+            A4.Stop();
+            A5.Stop();
+            A6.Stop();
+            A7.Stop();
+            A8.Stop();
+        }
+    }
+    void OpenInfo(InfoBehavior desiredInfo)
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            if (info == desiredInfo)
+            {
+                info.OpenInfo();
+            }
+            else
+            {
+                info.CloseInfo();
+            }
+        }
+    }
+
+    void CloseAll()
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            info.CloseInfo();
+        }
+    }
+}
---
+++ Assets/Asset Script/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform MenuUtama, MenuInformasi, MenuPetunjuk, Background;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuUtama.DOAnchorPos(new Vector2(0,0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(3000, 0), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(3000, 0), 1.0f);
+    }
+    public void MenuUtamabutton()
+    {
+        MenuUtama.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(3000, 0), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(3000, 0), 1.0f);
+    }
+    public void MenuInformasibutton()
+    {
+        MenuUtama.DOAnchorPos(new Vector2(3000, 0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(3000, 0), 1.0f);
+    }
+    public void MenuPetunjukbutton()
+    {
+        MenuUtama.DOAnchorPos(new Vector2(3000, 0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(3000, 0), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(0, 0), 1.0f);
+    }
+
+}
